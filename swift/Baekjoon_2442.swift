37b6c7360120720802c482cd0b1d193e043cb79b
Jiun Na <50474006+vonreal@users.noreply.github.com>
2021-12-23 17:52:27 +0900

    Create Baekjoon_2442.swift

---
+++ swift/Baekjoon_2442.swift
@@ -0,0 +1,21 @@
+let row = Int(readLine()!)!
+var str: String = ""
+var cnt: Int = 0
+
+for i in 1 ... row {
+    cnt = row - i
+    while (cnt > 0) {
+        str.append(" ")
+        cnt -= 1
+    }
+    for _ in 1 ... i {
+        str.append("*")
+    }
+    if (i >= 2) {
+        for _ in 2 ... i {
+            str.append("*")
+        }
+    }
+    print(str)
+    str = ""
+}
