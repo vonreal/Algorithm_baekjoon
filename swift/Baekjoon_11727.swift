ce66168e7ce2ade3d1276c2a05191fef56ea9e06
Jiun Na <50474006+vonreal@users.noreply.github.com>
2021-12-26 15:09:25 +0900

    Create Baekjoon_11727.swift

---
+++ swift/Baekjoon_11727.swift
@@ -0,0 +1,17 @@
+let n = Int(readLine()!)!
+var dp = Array<Int>(repeating: 0, count: n + 1)
+
+if n == 1 {
+    print("1")
+}
+else if n == 2 {
+    print("3")
+}
+else {
+    dp[1] = 1
+    dp[2] = 3
+    for i in 3 ... n {
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-2]) % 10007
+    }
+    print(dp[n])
+}
