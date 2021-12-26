872b54abbddb8ef7a65a6bd5e58d82f2b8d38e74
Jiun Na <50474006+vonreal@users.noreply.github.com>
2021-12-26 19:37:54 +0900

    Create Baekjoon_9498.swift

---
+++ swift/Baekjoon_9498.swift
@@ -0,0 +1,14 @@
+let point = Int(readLine()!)!
+
+switch point {
+case 90 ... 100:
+    print("A")
+case 80..<90:
+    print("B")
+case 70..<80:
+    print("C")
+case 60..<70:
+    print("D")
+default:
+    print("F")
+}
